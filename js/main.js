d161f04fb3b3846a661fd5f21fd0671c5b8be287
morozov-coder <54455134+morozov-coder@users.noreply.github.com>
2022-03-13 22:21:55 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const slider = new ChiefSlider('.about__slider', {
+    loop: true,
+    autoplay: true,
+    interval: 7000,
+  });
+});
 
 window.onscroll = function() {
   if (window.pageYOffset === 0) {
-    document.querySelector(".navbar").style.backgroundColor = "#1f2023";
+    document.querySelector(".navbar").classList.remove("scroll");
   } else {
-    document.querySelector(".navbar").style.backgroundColor = "#2d2f34";
+    document.querySelector(".navbar").classList.add("scroll");
   }
   
 }
+
+const bars = document.querySelector(".bars");
+bars.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  bars.classList.toggle("open");
+})
