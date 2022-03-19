0a8007280079f565da382b0ac71f10d9ed6fbc7b
Sergey <telkoffficial@gmail.com>
2022-03-19 23:41:25 +0300

    Added navigation

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     const slider = new ChiefSlider('.about__slider', {
-//         loop: true,
-//         autoplay: true,
-//         interval: 7000,
-//     });
-
-//     const portfolio = new ChiefSlider('.portfolio__slider', {
-//         loop: true,
-//         autoplay: false,
-//     })
-// });
-
 $(document).ready(() => {
     const slider = new ChiefSlider('.about__slider', {
         loop: true,
@@ -18,10 +5,10 @@ $(document).ready(() => {
         interval: 7000,
     });
 
-    const portfolio = new ChiefSlider('.portfolio__slider', {
-        loop: true,
-        autoplay: false,
-    })
+    // const portfolio = new ChiefSlider('.portfolio__slider', {
+    //     loop: true,
+    //     autoplay: false,
+    // })
 
     $(window).on('scroll', () => {
         if (window.pageYOffset === 0) {
@@ -35,20 +22,19 @@ $(document).ready(() => {
         evt.preventDefault();
         $(".bars").toggleClass("open");
     })
+
+    $('.navbar__link').on('click', (evt) => {
+        evt.preventDefault();
+        $('html, body').animate({ scrollTop: $("." + evt.target.id).offset().top - 100}, 500);
+    })
+
+    // $('.skills__item').on('mouseenter', (evt) => {
+    //     const $item = $(evt.target);
+    //     $item.addClass("skills__item--entered");
+    // });
+
+    // $('.skills__item').on('mouseleave', (evt) => {
+    //     const $item = $(evt.target);
+    //     $item.removeClass("skills__item--entered");
+    // })
 })
-
-
-
-// window.onscroll = function() {
-//     if (window.pageYOffset === 0) {
-//         document.querySelector(".navbar").classList.remove("scroll");
-//     } else {
-//         document.querySelector(".navbar").classList.add("scroll");
-//     }
-// }
-
-// const bars = document.querySelector(".bars");
-// bars.addEventListener("click", (evt) => {
-//     evt.preventDefault();
-//     bars.classList.toggle("open");
-// })
