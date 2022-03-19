97d168309908fa154b9f9380c57b2c6237a307bd
Sergey <telkoffficial@gmail.com>
2022-03-19 20:47:22 +0300

    JQuery added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function() {
+//     const slider = new ChiefSlider('.about__slider', {
+//         loop: true,
+//         autoplay: true,
+//         interval: 7000,
+//     });
+
+//     const portfolio = new ChiefSlider('.portfolio__slider', {
+//         loop: true,
+//         autoplay: false,
+//     })
+// });
+
+$(document).ready(() => {
     const slider = new ChiefSlider('.about__slider', {
         loop: true,
         autoplay: true,
@@ -9,18 +22,33 @@ document.addEventListener('DOMContentLoaded', function() {
         loop: true,
         autoplay: false,
     })
-});
 
-window.onscroll = function() {
-    if (window.pageYOffset === 0) {
-        document.querySelector(".navbar").classList.remove("scroll");
-    } else {
-        document.querySelector(".navbar").classList.add("scroll");
-    }
-}
+    $(window).on('scroll', () => {
+        if (window.pageYOffset === 0) {
+            document.querySelector(".navbar").classList.remove("scroll");
+        } else {
+            document.querySelector(".navbar").classList.add("scroll");
+        }
+    })
 
-const bars = document.querySelector(".bars");
-bars.addEventListener("click", (evt) => {
-    evt.preventDefault();
-    bars.classList.toggle("open");
+    const $bars = $(".bars").on("click", (evt) => {
+        evt.preventDefault();
+        $(".bars").toggleClass("open");
+    })
 })
+
+
+
+// window.onscroll = function() {
+//     if (window.pageYOffset === 0) {
+//         document.querySelector(".navbar").classList.remove("scroll");
+//     } else {
+//         document.querySelector(".navbar").classList.add("scroll");
+//     }
+// }
+
+// const bars = document.querySelector(".bars");
+// bars.addEventListener("click", (evt) => {
+//     evt.preventDefault();
+//     bars.classList.toggle("open");
+// })
